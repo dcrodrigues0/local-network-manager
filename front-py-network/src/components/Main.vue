b667dd512f34d80ef960293a96dd8ae99073e54a
Daniel Cavalcante <danielrcavalcante@gmail.com>
2020-04-10 03:28:24 -0300

    front-end py-home

---
+++ front-py-network/src/components/Main.vue
@@ -0,0 +1,104 @@
+<template>
+  <main>
+    <div class="black left-accordion">
+      <a href="#" class="main-icons icon-logo">
+        <i class="fas fa-paragraph"></i>
+      </a>
+      <a title="MAC Statistics" href="#" class="main-icons">
+        <i class="fas fa-chart-bar"></i>
+      </a>
+      <a title="My network package statistics" href="#" class="main-icons">
+        <i class="fas fa-wifi"></i>
+      </a>
+      <a title="MAC Statistics" href="#" class="main-icons">
+        <i class="fas fa-chart-pie"></i>
+      </a>
+    </div>
+    <div class="content-graphs">
+      <div class="graphs">
+        <ResizableScreen widthProp="350px" heightProp="400" title="MAC use"/>
+        <ResizableScreen widthProp="350px" heightProp="400" title="Network use"/>
+        <ResizableScreen widthProp="350px" heightProp="400" title="Trafic use"/>
+        <ResizableScreen widthProp="350px" heightProp="400" title="RAM use"/>
+      </div>
+    </div>
+  </main>
+</template>
+
+<script>
+  import ResizableScreen from '../tools/ResizableScreen'
+
+  export default {
+    name: 'app',
+    components: {
+      'ResizableScreen': ResizableScreen
+    }
+  }
+
+</script>
+
+
+
+<style>
+   main{
+     width: 100%;
+     height: 720px;
+     background-color: #161719;
+   }
+
+    .graphs {
+     display: flex;
+     flex-direction: row;
+     flex-wrap: wrap;
+     padding: 3%;
+   }
+
+   .content-graphs{
+     width: 100%;
+     height: auto;
+   }
+
+  .left-accordion{
+    width: 70px;
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+    height: 100%;
+    float: left;
+    box-shadow: 6px 0px 17px -4px rgba(0,0,0,0.56);
+  }
+
+  .left-accordion a:link{
+    color: #9F9F9F!important;
+  }
+
+  .left-accordion a{
+    padding: 10px;
+    color:#9F9F9F;
+  }
+
+  .left-accordion a:hover{
+    font-size: 36px;
+    transition: 0.2s;
+  }
+
+  .main-icons{
+    font-size: 32px;
+    position: relative;
+  }
+
+  .icon-logo{
+    position: absolute;
+    top: 0;
+    margin-top: 20px;
+    color:#ffab00!important;
+    font-size: 35px;
+  }
+
+  .icon-logo:hover{
+    transform: rotate(-360deg);
+    transition: 1s!important;
+  }
+
+</style>
---
+++ front-py-network/src/tools/ResizableScreen.vue
@@ -0,0 +1,48 @@
+<template>
+  <div :style="{'width':widthProp}" class="resizable-screen">
+    <p class="title-graph">{{title}}</p>
+  </div>
+</template>
+
+<script>
+  export default {
+    props: {
+      widthProp: {
+        type: String,
+        required: true,
+        default: "350px"
+      },
+      heightProp: {
+        type: String,
+        required: true,
+        default: "150px"
+      },
+      title: {
+        type: String,
+        default: 'Favor colocar titulo :)'
+      },
+
+    },
+  }
+
+</script>
+
+
+
+<style>
+
+  .title-graph{
+    margin: 5px;
+    color: lightgray;
+    font-weight: bold;
+  }
+
+   .resizable-screen{
+       /*width: 300px;*/
+       height: 150px;
+       background-color: #212124;
+       margin: 5px;
+   }
+</style>
+
+
