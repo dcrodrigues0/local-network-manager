84890aa42fba0884d82e5b946abae4c92ae9b7ea
lecoliveira <lecoliveira@uolinc.com>
2020-05-08 00:17:45 -0300

    Formatando retorno

--- container/services/sniffer/service.py
+++ container/services/sniffer/service.py
@@ -7,8 +7,20 @@ class Service(mongo.MongoDAO):
 
 
     def getTrafficByDate(self,date):
-         return util.toJson(self.getTrafficByDay(date))
+         return self.makeDataGraph(self.getTrafficByDay(date), 'date', 'avg')
 
 
     def getTrafficHourByDate(self, date):
-        return util.toJson(self.getTrafficHour(date))
+        return self.makeDataGraph(self.getTrafficHour(date), 'hour', 'date')
+
+
+    def makeDataGraph(self, value, campoLabel, campoDataset):
+        labels = []
+        dataset = []
+
+        for doc in value:
+            labels.append(doc[campoLabel])
+            dataset.append(doc[campoDataset])
+
+        return {'labels': labels, 'dataset': { 'data': dataset}}
+
