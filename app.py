cd2ec5cf092c598bafb9884b4102dff383f6e41f
Daniel Cavalcante <danielrcavalcante@gmail.com>
2020-05-09 02:12:22 -0300

    fix CORS problem with browsers

--- app.py
+++ app.py
@@ -1,8 +1,12 @@
 from flask import Flask
 from container.endpoints.routes import routes
 from dotenv import load_dotenv
+from flask_cors import CORS,cross_origin
 
 app = Flask(__name__)
+CORS(app)
+
+
 app.register_blueprint(routes)
 load_dotenv()
 
