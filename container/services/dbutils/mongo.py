f4122aaa078bda696b7783ed253d5852b86cd5c9
Rafael <rafael@pop-os.localdomain>
2020-05-24 23:54:16 -0300

    Add colours to terminal messages

--- container/services/dbutils/mongo.py
+++ container/services/dbutils/mongo.py
@@ -1,51 +1,54 @@
 from container.database import connection as conn
+import container.log.Colorer
+import logging
 
 
 class MongoDAO():
     def __init__(self):
         self._conn = conn.Connection()
         self.initConnection()
+        logging.basicConfig(level=logging.DEBUG)
 
     def initConnection(self):
         self._db = self._conn.getDB()
 
     def saveTrafficFull(self, json):
-        print("Start Insert full network traffic")
+        logging.info("Start Insert full network traffic")
         self._db.trafficfull.insert_many(json)
-        print("Finish Insert full network traffic")
+        logging.info("Finish Insert full network traffic")
 
     def saveTrafficMac(self, json):
-        print("Start Insert Mac network traffic")
+        logging.info("Start Insert Mac network traffic")
         self._db.trafficMac.insert(json)
-        print("Finish Insert Mac network traffic")
+        logging.info("Finish Insert Mac network traffic")
 
     def saveTrafficDay(self, json):
-        print("Start Insert day network traffic")
+        logging.info("Start Insert day network traffic")
         self._db.trafficIntraday.insert(json)
-        print("Finish Insert day network traffic")
+        logging.info("Finish Insert day network traffic")
 
     def saveTrafficHourDay(self, json):
-        print("Start Insert hour by day network traffic")
+        logging.info("Start Insert hour by day network traffic")
         self._db.trafficHour.insert(json)
-        print("Finish Insert hour by day network traffic")
+        logging.info("Finish Insert hour by day network traffic")
 
     def saveRealTimeTraffic(self, json):
-        print("Start Insert realtime traffic")
+        logging.info("Start Insert realtime traffic")
         self._db.realTime.insert(json)
-        print("Finish Insert realtime traffic")
+        logging.info("Finish Insert realtime traffic")
 
     def saveTrafficIpAddress(self, json):
-        print("Start Insert packets by IP address traffic")
+        logging.info("Start Insert packets by IP address traffic")
         self._db.trafficIp.insert(json)
-        print("Finish Insert packets by IP address traffic")
+        logging.info("Finish Insert packets by IP address traffic")
 
     def saveRealTimeTable(self, json):
-        print("Start Insert traffic real time table")
+        logging.info("Start Insert traffic real time table")
         self._db.trafficTable.insert(json)
-        print("Finish Insert traffic real time table")
+        logging.info("Finish Insert traffic real time table")
 
     def updateIntradayMongo(self, id, json):
-        print("Update intraday")
+        logging.info("Update intraday")
         self._db.trafficIntraday.update(
             {"_id": id},
             {"$set":
@@ -55,7 +58,7 @@ class MongoDAO():
              }, upsert=True)
 
     def updateHourMongo(self, json):
-        print("Update Hour")
+        logging.info("Update Hour")
         self._db.trafficHour.update(
             {"_id": json['_id']},
             {"$set":
@@ -63,7 +66,7 @@ class MongoDAO():
              }, upsert=True)
 
     def updateTrafficByIpAddr(self, id, json):
-        print("Update IP Addresses")
+        logging.info("Update IP Addresses")
         self._db.trafficIp.update(
             {"_id": id},
             {"$set":
